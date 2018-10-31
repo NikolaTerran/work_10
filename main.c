b41265d567cb91ff940bde8829fb0183f96a33b4
tianrunliu <tliu3@stuy.edu>
2018-10-30 22:12:53 -0400

    fixed bug

--- main.c
+++ main.c
@@ -26,7 +26,7 @@ int main(){
 
 
 	printf("write it to file and print the result\n");
-	int des = open("virus.txt",O_RDWR,777);
+	int des = open("virus.txt",O_RDWR|O_CREAT,0777);
         int whut = write(des,buffer,size * sizeof(int));
 	close(des);
 
@@ -41,6 +41,6 @@ int main(){
 		printf("%d\n",new_buff[i]);
 	}
 	close(des);
-	printf("for some reason, if you run it multiple times, the result of the last run will be erased from virus.txt. And you have to name the file \"virus.txt\" not simply as \"virus\" ¯\\_(ツ)_/¯\n");
+	printf("¯\\_(ツ)_/¯\n");
         return 0;
 }
